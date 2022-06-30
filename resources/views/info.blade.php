652ddd645b9a17cf6febe155375b7e395c85f8c2
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-30 15:34:40 +0300

    added page title to main template.

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,4 +1,7 @@
 @extends('main')
+@section('title')
+Info
+@endsection
 @section('content')
 <p>Info about this website</p>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,7 @@
 @extends('main')
+@section('title')
+Learning Laravel
+@endsection
 @section('content')
 <p>Welcome page</p>
 @endsection
